d457506a61373337d9d66b2eec493c83aab70ce5
Priya <sreepdsy1994@gmail.com>
2022-01-12 22:27:27 -0500

    Change variable name for refill counts

--- src/loopAssignment1/Question2.java
+++ src/loopAssignment1/Question2.java
@@ -13,22 +13,24 @@ public class Question2 {
 
 		System.out.print("Enter the maximum capacity of the tank in litres :	");
 		int tank_Cap = inputVol.nextInt(); // input 100 for this question
+		
 		System.out.print("Enter the maximum capacity of the bucket in litres :	");
 		int bucket_Cap = inputVol.nextInt(); // input 10 for this question
+		
 		System.out.print("Enter the current volume in the tank :	");
 		double currentVol = inputVol.nextDouble();
-
-		int maxFill = tank_Cap / bucket_Cap;
-		int countFill = 0;
-		while (countFill <= maxFill) {
-			if (currentVol > (tank_Cap - bucket_Cap)) {
+		
+		int maxFill = tank_Cap / bucket_Cap;  //variable to know what is the maximum times the tank needs to be filled 
+		int fill_Counter = 0;	//variable to count how many times tank is filled using bucket
+		while (fill_Counter <= maxFill) {
+			if (currentVol >= (tank_Cap - bucket_Cap)) {
 				break;
 			} 
 			currentVol += bucket_Cap;
-			countFill++;
+			fill_Counter++;
 		}
 		
-		System.out.println("Total number of fills using bucket : " + countFill);
+		System.out.println("Total number of fills using bucket : " + fill_Counter);
 		System.out.println("Current Volume is "+currentVol+ " litres. Further filling causes overflowing");
 		
 		inputVol.close();
