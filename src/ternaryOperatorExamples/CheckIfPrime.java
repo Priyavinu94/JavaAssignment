3601f503e303c02494871596174e62258a9d877f
Priya <sreepdsy1994@gmail.com>
2022-01-14 21:21:33 -0500

    improve code

--- src/ternaryOperatorExamples/CheckIfPrime.java
+++ src/ternaryOperatorExamples/CheckIfPrime.java
@@ -14,15 +14,13 @@ public class CheckIfPrime {
 		boolean isPrime = true;
 		// if num is prime, it is completely divisible by 1 and num only..
 		// below loop checks if each integer from 2 to num/2 is a divisor of num or not.
-		// we check until num/2, because, for any number, it is not divisible by more
-		// than its half
+		// we check until num/2, because, for any number, it is not divisible by more than its half
 		for (int div = 2; div <= num / 2; div++) {
 			if (num % div == 0) {
 				isPrime = false;
 				break;
 			}
 		}
-
 		System.out.println((isPrime==true) ? "This is a Prime No" : "This is not a Prime No");
 		inputInt.close();
 
